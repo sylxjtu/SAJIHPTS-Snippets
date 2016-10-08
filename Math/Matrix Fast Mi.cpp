7497d6e0c3099668df4ecabb4f2f6af0a1eb0dc3
igoodvegetablea <lzy960601@outlook.com>
2016-10-08 17:59:11 +0800

    English names

---
+++ Math/Matrix Fast Mi.cpp
@@ -0,0 +1,51 @@
+﻿struct mat
+{
+    int n;
+    LL num[105][105];
+
+    void init0(int t)
+    {
+        n=t;
+        for(int i=0;i<=n;i++)
+            for(int j=0;j<=n;j++)
+                num[i][j]=0;
+    }
+
+    void init1(int t)
+    {
+        n=t;
+        for(int i=0;i<=n;i++)
+            for(int j=0;j<=n;j++)
+                if(i!=j)num[i][j]=0;else num[i][j]=1;
+    }
+
+    mat operator * (const struct mat p)const
+    {
+        struct mat ans;
+        ans.init0(n);
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=n;j++)
+                for(int k=1;k<=n;k++)
+                    ans.num[i][j]=(ans.num[i][j]+num[i][k]*p.num[k][j])%mod;
+        //printf("??");ans.testprint();
+        return ans;
+    }       
+
+    mat operator ^ (int t)const
+    {
+        struct mat ans,now;
+        ans.init1(n);
+        now.n=n;
+        for(int i=0;i<=n;i++)
+            for(int j=0;j<=n;j++)
+                now.num[i][j]=num[i][j];
+        while(t>0)
+        {
+            if(t&1)ans=ans*now;
+            now=now*now;
+            t>>=1;
+        }
+        return ans;
+    }
+
+};
