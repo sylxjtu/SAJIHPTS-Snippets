0288797d53a42b0575d780243a9a9506d2cdf0f7
sylxjtu <a443833359@gmail.com>
2016-10-20 19:57:27 +0800

    print1020

--- print.py
+++ print.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import sys
 import shutil
+from collections import OrderedDict
 
 header = r"""
 \documentclass{article}
@@ -17,7 +18,7 @@ header = r"""
 \setmonofont{Fantasque Sans Mono}
 \setCJKmainfont{Microsoft YaHei}
 \setCJKmonofont{Microsoft YaHei}
-\setminted[cpp]{linenos, breaklines}
+\setminted[cpp]{linenos, breaklines, tabsize=2}
 
 \title{SAJIHPTS Code Snippets}
 \date{\today}
@@ -38,34 +39,46 @@ footer = r"""
 os.chdir(sys.path[0])
 
 if os.path.exists("dist"):
-    shutil.rmtree("dist")
+	shutil.rmtree("dist")
 os.makedirs("dist")
 os.chdir("dist")
 
-dic = {}
+dic = OrderedDict({})
+dicT = OrderedDict({})
 
 def texencode(s):
 	return s.replace('&', r'\&').replace('_', r'\_')
 
 for root, dirs, files in os.walk('..', topdown=True):
-    dirs[:] = [d for d in dirs if d[0] != '.']
-    for f in files:
-        if(f.endswith(".cpp")):
-            dic.setdefault(root, []).append(f)
+	dirs[:] = [d for d in dirs if d[0] != '.']
+	for f in files:
+		if(f.endswith(".cpp")):
+			dic.setdefault(root, []).append(f)
+		if(f.endswith(".txt")):
+			dicT.setdefault(root, []).append(f)
 
 with open('out.tex', 'w', encoding="utf8") as fh:
-    fh.write("% Auto Generated on " + datetime.datetime.today().isoformat(' '))
-    fh.write(header)
-    for d in dic:
-        fh.write("""
-        \\section{%s}
-        """ % texencode(d.split('\\')[-1]))
-        for f in dic[d]:
-            fh.write("""
-            \\subsection{%s}
-            \\inputminted{cpp}{%s}
-            """ % (texencode(f.split('.')[0]), '"' + texencode(os.path.join(d, f).replace('\\', '/')) + '"'))
-    fh.write(footer)
+	fh.write("% Auto Generated on " + datetime.datetime.today().isoformat(' '))
+	fh.write(header)
+	for d in dic:
+		fh.write("""
+		\\section{%s}
+		""" % texencode(d.split('\\')[-1]))
+		for f in dic[d]:
+			fh.write("""
+			\\subsection{%s}
+			\\inputminted{cpp}{%s}
+			""" % (texencode(f.split('.')[0]), '"' + texencode(os.path.join(d, f).replace('\\', '/')) + '"'))
+	for d in dicT:
+		fh.write("""
+		\\section{%s}
+		""" % texencode(d.split('\\')[-1]))
+		for f in dicT[d]:
+			fh.write("""
+			\\subsection{%s}
+			\\inputminted{text}{%s}
+			""" % (texencode(f.split('.')[0]), '"' + texencode(os.path.join(d, f).replace('\\', '/')) + '"'))
+	fh.write(footer)
 
 os.system("xelatex -shell-escape out.tex")
 os.system("xelatex -shell-escape out.tex")
